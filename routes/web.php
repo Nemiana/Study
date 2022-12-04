bc59f69da9809e080a1a868c56325a948fd2c6f1
Nemiana <neminemiana@gmail.com>
2022-12-04 20:45:48 +0200

    controller route groups

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(\App\Http\Controllers\PostsController::class)->group(function () {
+    Route::get('/posts', 'index');
+    Route::get('/posts/{post}', 'show');
+    Route::post('/posts', 'store');
+});
+
+
